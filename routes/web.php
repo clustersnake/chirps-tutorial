b890f8fc202fc20f887b726e9a4aa0fef5cb2d41
Eloy Ramirez <clustersnake45@gmail.com>
2023-11-21 08:33:41 -0400

    feat: Ajustar rutas en grupo auth

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get(
+        '/chirps',
+        function () {
+            return 'Chirps Index';
+        }
+    )->name('chirps.index');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
