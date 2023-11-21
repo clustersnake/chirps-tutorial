84755c4b83a8797ea6b981a6df76ab4f8375fadc
Eloy Ramirez <clustersnake45@gmail.com>
2023-11-21 13:20:31 -0400

    feat: Agregar enlaces de navegacion

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get(
+    Route::view(
         '/chirps',
-        function () {
-            return 'Chirps Index';
-        }
+        'chirps.index'
     )->name('chirps.index');
 });
 
